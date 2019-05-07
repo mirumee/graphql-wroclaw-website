952b00e15c987fc8a8dac7bd227c41350985a56f
Lukasz Szewczyk <szewczyk134@gmail.com>
2019-05-07 11:56:18 +0200

    GatsbyJS init

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `GrapgQL Wroclaw`,
+    description: `Events about GraphQL`,
+    author: `@mirumee`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/icons/index.js
@@ -0,0 +1,21 @@
+import Youtube from "./youtube-logo.svg";
+import GraphQLWroclaw from "./graphql-wroclaw-logo.svg";
+import Saleor from "./saleor-logo.svg";
+import Facebook from "./facebook-logo.svg";
+import Meetup from "./meetup-logo.svg";
+import Mirumee from "./mirumee-logo.svg";
+import PoweredByMirumee from "./poweredbymirumee-logo.svg";
+import Arrow from "./arrow.svg";
+
+const Logo = {};
+
+Logo.Youtube = Youtube;
+Logo.GraphQLWroclaw = GraphQLWroclaw;
+Logo.Meetup = Meetup;
+Logo.Saleor = Saleor;
+Logo.Facebook = Facebook;
+Logo.Mirumee = Mirumee;
+Logo.PoweredByMirumee = PoweredByMirumee;
+Logo.Arrow = Arrow;
+
+export default Logo;
