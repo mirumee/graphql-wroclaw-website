f7ea427018499f5f034b987bc4151b39e7dee93e
Bonifacy1 <szewczyk134@gmail.com>
2019-05-17 16:09:11 +0200

    Adjust bourger menu

--- src/components/navbar/styles/Line.js
+++ src/components/navbar/styles/Line.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 const Line = styled.div`
-  height: 2px;
+  height: 3px;
   width: 23px;
   width: ${props => props.bottom && !props.menuOpen && `12px`};
   width: ${props => props.top && !props.menuOpen && `16px`};
@@ -11,22 +11,24 @@ const Line = styled.div`
         ? props => props.theme.colors.primaryColor
         : props => props.theme.colors.mainBrandColor};
   transition: all 0.2s ease;
-  margin-bottom: ${props => (props.top ? `3px` : 0)};
-  margin-top: ${props => (props.bottom ? `3px` : 0)};
   transform-origin: left;
   transform: ${props =>
-    (props.top && props.menuOpen && `rotate(30deg)`) ||
-    (props.bottom && props.menuOpen && `rotate(-30deg)`)};
+    (props.top && props.menuOpen && `rotate(34deg)`) ||
+    (props.bottom && props.menuOpen && `rotate(-34deg)`)};
   transform: ${props => props.middle && props.menuOpen && `translateX(-30px)`};
   opacity: ${props => (props.middle && props.menuOpen ? 0 : 1)};
+  background-color: ${props =>
+    props.middle || props.menuOpen
+      ? props.theme.colors.mainBrandColor
+      : props.theme.colors.primaryColor};
 
   ${props => props.theme.media.laptop`
   width: 28px;
   width: ${props => props.bottom && !props.menuOpen && `15px`};
   width: ${props => props.top && !props.menuOpen && `20px`};;
   transform: ${props =>
-    (props.top && props.menuOpen && `rotate(26deg)`) ||
-    (props.bottom && props.menuOpen && `rotate(-26deg)`)};`}
+    (props.top && props.menuOpen && `rotate(29deg)`) ||
+    (props.bottom && props.menuOpen && `rotate(-29deg)`)};`}
 `
 
 export default Line
--- src/components/navbar/styles/MenuContainer.js
+++ src/components/navbar/styles/MenuContainer.js
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 const MenuContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-  align-items: center;
+  align-items: flex-start;
   width: 378px;
-`;
+`
 
-export default MenuContainer;
+export default MenuContainer
