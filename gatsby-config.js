9d136fa0252da50d067e1bc71ddd515732deda2a
Bonifacy1 <szewczyk134@gmail.com>
2019-05-20 17:22:50 +0200

    Add github-pg

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/graphql-wroclaw-website",
   siteMetadata: {
     title: `GrapgQL Wroclaw`,
     description: `Events about GraphQL`,
@@ -13,9 +14,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/data/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
+    "gatsby-transformer-json",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
