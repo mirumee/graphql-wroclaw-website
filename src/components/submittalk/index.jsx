a88deaf44d9ecdd2806a1f26f88fead38e3944ad
Rafał Pitoń <rafal@mirumee.com>
2019-07-16 16:20:45 +0200

    Commit missing file

---
+++ src/components/submittalk/index.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const SubmitTalk = styled.div`
+  background-color: ${props => props.theme.colors.thirdColor};
+  color: ${props => props.theme.colors.secondaryColor};
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 3rem 0rem;
+  ${props => props.theme.media.tablet`padding: 3rem 2rem`}
+
+  h2 {
+    font-size: 2.25rem;
+    margin-bottom: 1.5rem;
+    padding: 0;
+    ${props => props.theme.media.tablet`padding: 0rem 1rem`}
+  }
+`
+
+export default SubmitTalk
